6c1cb1eb1206321b7ba71c691ee09370b9cd313b
sbx_user1051 <sbx_user1051@169.254.165.213>
2019-04-04 16:05:01 +0000

    Thu Apr 04 16:05:01 UTC 2019

--- test/solution_tests/FIZ/test_fizz_buzz.py
+++ test/solution_tests/FIZ/test_fizz_buzz.py
@@ -2,10 +2,19 @@ from solutions.FIZ import fizz_buzz_solution
 
 
 class TestFizzBuzz():
-    def test_fizz(self):
+    def test_3_is_fizz(self):
         assert fizz_buzz_solution.fizz_buzz(3) == "fizz"
 
-    def test_buzz(self):
+    def test_13_is_fizz(self):
+        assert fizz_buzz_solution.fizz_buzz(13) == "fizz"
+
+    def test_3091_is_fizz(self):
+        assert fizz_buzz_solution.fizz_buzz(3091) == "fizz"
+
+    def test_5_is_buzz(self):
+        assert fizz_buzz_solution.fizz_buzz(5) == "buzz"
+
+    def test_50_is_buzz(self):
         assert fizz_buzz_solution.fizz_buzz(5) == "buzz"
 
     def test_fizz_buzz_for_15(self):
@@ -19,3 +28,4 @@ class TestFizzBuzz():
 
     def test_7(self):
         assert fizz_buzz_solution.fizz_buzz(7) == "7"
+
