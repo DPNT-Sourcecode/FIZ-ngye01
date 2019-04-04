35235532f2f9f43d0c40360e733da2727099bc22
sbx_user1051 <sbx_user1051@169.254.165.213>
2019-04-04 16:01:59 +0000

    Thu Apr 04 16:01:59 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,7 +1,11 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    if number % 3 == 0:
+    if number % 15 == 0:
+        return "fizz buzz"
+    elif number % 3 == 0:
         return "fizz"
     elif number % 5 == 0:
         return "buzz"
+    return str(number)
     
+
