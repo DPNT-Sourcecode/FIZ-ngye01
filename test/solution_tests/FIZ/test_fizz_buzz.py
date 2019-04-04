def74450d73315d6785ce7fe0a01be0df1527b03
sbx_user1051 <sbx_user1051@169.254.165.213>
2019-04-04 15:57:46 +0000

    Thu Apr 04 15:57:46 UTC 2019

--- test/solution_tests/FIZ/test_fizz_buzz.py
+++ test/solution_tests/FIZ/test_fizz_buzz.py
@@ -2,7 +2,14 @@ from solutions.FIZ import fizz_buzz_solution
 
 
 class TestFizzBuzz():
-    def test_fizz_buzz(self):
-        assert fizz_buzz_solution.fizz_buzz("John") == "fizz_buzz, John!"
-        # assert fizz_buzz_solution.fizz_buzz("Alice") == "fizz_buzz, Alice!"
+    def test_fizz(self):
+        assert fizz_buzz_solution.fizz_buzz(3) == "fizz"
 
+    def test_buzz(self):
+        assert fizz_buzz_solution.fizz_buzz(5) == "buzz"
+
+    def test_fizz_buzz_for_15(self):
+        assert fizz_buzz_solution.fizz_buzz(15) == "fizz buzz"
+
+    def test_fizz_buzz_for_30(self):
+        assert fizz_buzz_solution.fizz_buzz(30) == "fizz buzz"
