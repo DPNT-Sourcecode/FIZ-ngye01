dddb737bf66f83c86fc6a412e49898e39ac89e2e
sbx_user1051 <sbx_user1051@169.254.165.213>
2019-04-04 15:35:53 +0000

    Thu Apr 04 15:35:53 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,5 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    3
+    return x + y
+
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,3 +4,9 @@ from solutions.SUM import sum_solution
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(1, 5) == 6
+
+    def test_sum(self):
+        assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(1, 5) == 6
+
